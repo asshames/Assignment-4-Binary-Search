52e16e6391c8d28014638c3c52e4c0d81ef04f59
Shames <assamialomarshames@gmail.com>
2018-12-17 01:10:10 +0600

    built main function for selectionsort in array base

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,4 +1,7 @@
 #include "SelectionSort.h"
+#include <iostream>
+
+using namespace std;
 
 SelectionSort::SelectionSort()
 {
@@ -10,7 +13,7 @@ SelectionSort::~SelectionSort()
 
 }
 
-inline void Swap(int &first. int &second)
+inline void Swap(int &first, int &second)
 {
     int temp = first;
     first = second;
@@ -19,12 +22,18 @@ inline void Swap(int &first. int &second)
 
 void SelectionSort::SelectionSorter(int arr[], int arr_size)
 {
+    cout<<"Showing Here Every Step for Selection Sort in Array Base"<<endl;
     for(int index = 0; index < arr_size-1; ++index)
     {
         int Min = index;
-        for(int index2 = index+1; index2< arr_size; ++index2)
+        for(int index2 = index+1; index2<arr_size; ++index2)
             if (arr[index2] < arr[Min])
             Min = index2;
         Swap(arr[Min], arr[index]);
+        for(int index = 0; index<arr_size; index++)
+        {
+            cout<<arr[index]<<" ";
+        }
+        cout<<""<<endl;
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include "SelectionSort.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout<<"\n Array Base \n"<<endl;
+    int arr[]={14,33,27,10,35,19,48,44};
+    int arraySelectionSort[8];
+    for(int index=0; index<8; index++)
+    {
+        arraySelectionSort[index] = arr[index];
+    }
+    SelectionSort ObjectSelection;
+    ObjectSelection.SelectionSorter(arraySelectionSort,8);
+    cout<<"Selection Sort Final Output for Array Base : " <<endl;
+    for(int index=0; index<8; index++)
+    {
+        cout<<arraySelectionSort[index]<<" ";
+    }
+    cout<<"\n\n\n"<<endl;
+
+
     return 0;
 }
