da894d3bbd92651815968142c634c51535c4f95b
Shames <assamialomarshames@gmail.com>
2018-12-17 00:27:05 +0600

    Write function for Selection Sort array

--- SelectionSort.h
+++ SelectionSort.h
@@ -1,6 +1,14 @@
 #ifndef SELECTIONSORT_H_INCLUDED
 #define SELECTIONSORT_H_INCLUDED
 
+class SelectionSort{
+
+public:
+    SelectionSort();
+    ~SelectionSort();
+    void SelectionSort(int arr[], int arr_size);
+
+};
 
 
 #endif // SELECTIONSORT_H_INCLUDED
