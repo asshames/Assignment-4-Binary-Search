4df8bafcb3ebdb787115844146d48ffb17ccf61c
Shames <assamialomarshames@gmail.com>
2018-12-20 00:07:53 +0600

    built header filer for Insertion Sort

--- InsertionSort.h
+++ InsertionSort.h
@@ -1,6 +1,20 @@
 #ifndef INSERTIONSORT_H_INCLUDED
 #define INSERTIONSORT_H_INCLUDED
 
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class InsertionSort
+{
+public:
+    InsertionSort();
+    ~InsertionSort();
+    void InsertionSorter(int arr[], int arr_size);
+    void InsertionSorterVector(vector<int>& data);
+};
+
 
 
 #endif // INSERTIONSORT_H_INCLUDED
