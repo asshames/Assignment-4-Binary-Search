56b9f52651ab0284f416fd75584ee1a914f2610a
Shames <assamialomarshames@gmail.com>
2018-12-20 00:38:10 +0600

    built the cpp file

---
+++ InsertionSort.cpp
@@ -0,0 +1,64 @@
+#include "InsertionSort.h"
+#include<iostream>
+using namespace std;
+
+InsertionSort::InsertionSort()
+{
+
+}
+
+InsertionSort::~InsertionSort()
+{
+
+}
+
+void InsertionSort::InsertionSorter(int arr[], int arr_size)
+{
+    cout<<"Insertion Sort Steps for Array::"<<endl;
+    if(arr_size > 1)
+    {
+        int size = arr_size;
+        for(int index = 1; index < size; ++index)
+        {
+            int number = index - 1;
+            int key = arr[index];
+            while(number >= 0 && arr[number] > key)
+            {
+                arr[number+1] = arr[number];
+                --number;
+            }
+
+            arr[number+1] = key;
+
+            for(int index2=0; index2<arr_size; index2++)
+            {
+                cout<<arr[index2]<<" ";
+            }
+            cout<<""<<endl;
+        }
+    }
+}
+
+void InsertionSort::InsertionSorterVector(vector<int>& data)
+{
+    cout<<"Insertion Sort Steps for Vector::"<<endl;
+    if(data.size() > 1)
+    {
+        for(int index = 1; index< data.size(); index++)
+        {
+            int number = index- 1;
+            int key = data[index];
+            while(number>= 0 && data[number] > key)
+            {
+                data[number+1] = data[number];
+                number--;
+            }
+
+            data[number+1] = key;
+
+            for (vector<int>::iterator value = data.begin();value != data.end(); ++value)
+                cout << *value <<" ";
+                cout<<""<<endl;
+        }
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include "SelectionSort.h"
+#include "InsertionSort.h"
 
 using namespace std;
 
 int main()
 {
-    cout<<"\n Array Base \n"<<endl;
     int arr[]={14,33,27,10,35,19,48,44};
     int arraySelectionSort[8];
+    cout<<"\nArray Base"<<endl;
     for(int index=0; index<8; index++)
     {
         arraySelectionSort[index] = arr[index];
@@ -28,6 +29,31 @@ int main()
     for(int index=0;index<vectorSelectionSort.size();index++)
         cout<<vectorSelectionSort[index]<<" ";
     cout<<"\n"<<endl;
+    cout<<"----------------------------------------------------------------------------------------"<<endl;
+    int arrayInsertionSort[8];
+    cout<<"\nArray Base"<<endl;
+    for(int index=0; index<8; index++)
+    {
+        arrayInsertionSort[index] = arr[index];
+    }
+    InsertionSort ObjectInsertion;
+    ObjectInsertion.InsertionSorter(arrayInsertionSort,8);
+     cout<<"Insertion Sort Final Output for Array Base : "<<endl;
+    for(int index=0; index<8; index++)
+    {
+        cout<<arrayInsertionSort[index]<<" ";
+    }
+    cout<<"\n\n\n"<<endl;
+    cout<<"Vector Part"<<endl;
+    std::vector<int> vectorInsertionSort(arr,arr+sizeof(arr)/sizeof(int));
+    ObjectInsertion.InsertionSorterVector(vectorInsertionSort);
+    cout<<"Insertion Sort Final Output for Vector : "<<endl;
+    for(int index=0; index<vectorInsertionSort.size(); index++)
+        cout<<vectorInsertionSort[index]<<" ";
+    cout<<"\n"<<endl;
+    cout<<"----------------------------------------------------------------------------------------"<<endl;
+
+
 
     return 0;
 }
